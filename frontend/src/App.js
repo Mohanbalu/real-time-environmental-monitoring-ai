69e31ab3fdcff683eac263198e2b1c6b56432077
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2025-12-27 17:24:30 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Alerts from "./pages/Alerts";
+import Settings from "./pages/Settings";
 
-function App() {
-  return <Dashboard />;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/alerts" element={<Alerts />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
