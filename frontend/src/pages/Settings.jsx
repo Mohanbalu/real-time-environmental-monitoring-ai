757fe7b99774be67453ecc2b7789336858faa460
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2025-12-27 16:57:57 +0530

    Update Settings.jsx

---
+++ frontend/src/pages/Settings.jsx
@@ -0,0 +1,14 @@
+import RuleForm from "../components/rules/RuleForm";
+import RuleList from "../components/rules/RuleList";
+
+const Settings = () => {
+  return (
+    <div>
+      <h2>Threshold Settings</h2>
+      <RuleForm />
+      <RuleList />
+    </div>
+  );
+};
+
+export default Settings;
