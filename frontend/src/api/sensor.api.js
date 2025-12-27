de5a57cf3708223b744578864a4860e5be2008a1
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2025-12-27 15:45:36 +0530

    Update sensor.api.js

--- frontend/src/api/sensor.api.js
+++ frontend/src/api/sensor.api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/sensors";
+const API_URL =
+  "https://real-time-environmental-monitoring-ai.onrender.com/api/sensors";
 
 export const fetchRecentData = async () => {
   const res = await axios.get(`${API_URL}/recent`);
