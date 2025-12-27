9a598f4dd1d7ced5fb01621c55b79540b63e5f61
Mohan Balu <mohanbalu292@gmail.com>
2025-12-27 18:44:07 +0530

    remove Navbar usage from App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Alerts from "./pages/Alerts";
 import Dashboard from "./pages/Dashboard";
-import Settings from "./pages/Settings";
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/alerts" element={<Alerts />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+function App() {
+  return <Dashboard />;
+}
 
 export default App;
