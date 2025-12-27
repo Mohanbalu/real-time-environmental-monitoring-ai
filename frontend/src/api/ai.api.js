6e619b89882b4cc008768dcf411fc43127ec374f
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2025-12-27 15:51:47 +0530

    Update ai.api.js

--- frontend/src/api/ai.api.js
+++ frontend/src/api/ai.api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const BASE_URL =
+  "https://real-time-environmental-monitoring-ai.onrender.com";
+
 export const fetchAIInsights = async () => {
-  const res = await axios.get("http://localhost:5000/api/ai/insights");
+  const res = await axios.get(`${BASE_URL}/api/ai/insights`);
   return res.data;
 };
