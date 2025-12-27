10b39406438c572a159a32a3e20d060da34521ca
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2025-12-27 16:57:40 +0530

    Add Alerts component with AlertList

---
+++ frontend/src/pages/Alerts.jsx
@@ -0,0 +1,12 @@
+import AlertList from "../components/alerts/AlertList";
+
+const Alerts = () => {
+  return (
+    <div>
+      <h2>Alerts</h2>
+      <AlertList />
+    </div>
+  );
+};
+
+export default Alerts;
