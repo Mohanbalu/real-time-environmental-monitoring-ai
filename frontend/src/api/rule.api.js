5a020fd0d5e19fb3a26d1465bfd54a42c1d34005
Mohanbalu <122850324+Mohanbalu@users.noreply.github.com>
2025-12-27 16:51:21 +0530

    Update API URL for rule fetching and saving

--- frontend/src/api/rule.api.js
+++ frontend/src/api/rule.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/rules";
+const API_URL = "https://real-time-environmental-monitoring-ai.onrender.com/rules";
 
 export const fetchRule = async () => {
   const res = await axios.get(API_URL);
@@ -11,3 +11,4 @@ export const saveRule = async (rule) => {
   const res = await axios.post(API_URL, rule);
   return res.data;
 };
+
